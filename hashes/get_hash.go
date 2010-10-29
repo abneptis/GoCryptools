4a04d92f517e2816446e62fe94d7bf340ce39433
James D. Nurmi <james@auspicious.abneptis.net>
2010-10-29 14:13:19 -0700

    Initial commit of hash-finding and hmac-finding functions;
    Also implements a basic interface for Signers

---
+++ hashes/get_hash.go
@@ -0,0 +1,47 @@
+package hashes
+
+import "crypto/hmac"
+import "crypto/md4"
+import "crypto/md5"
+import "crypto/sha1"
+import "crypto/sha256"
+import "crypto/sha512"
+
+import "hash"
+import "strings"
+import "os"
+
+var Md4  = func()(hash.Hash){ return md4.New() }
+var Md5  = func()(hash.Hash){ return md5.New() }
+var Sha1 = func()(hash.Hash){ return sha1.New() }
+var Sha256 = func()(hash.Hash){ return sha256.New() }
+var Sha384 = func()(hash.Hash){ return sha512.New384() }
+var Sha512 = func()(hash.Hash){ return sha512.New() }
+var ErrUnknownHash = os.NewError("Unknown Hash")
+
+func GetHashFunc(n string)(hf func()(hash.Hash), err os.Error){
+  switch strings.ToLower(n) {
+    case "md4": hf =  Md4
+    case "md5": hf =  Md4
+    case "sha1": hf =  Sha1
+    case "sha256": hf =  Sha256
+    case "sha384": hf =  Sha384
+    case "sha512": hf =  Sha512
+    default: err = ErrUnknownHash
+  }
+  return
+}
+
+func GetHash(n string)(h hash.Hash, err os.Error){
+  f, err := GetHashFunc(n)
+  if err != nil { return }
+  h = f()
+  return
+}
+
+func GetHmac(n string, key []byte)(h hash.Hash, err os.Error){
+  hf, err := GetHashFunc(n)
+  if err != nil { return }
+  h = hmac.New(hf, key)
+  return
+}
---
+++ signer/signer.go
@@ -0,0 +1,39 @@
+package signer
+
+import "bytes"
+import "encoding/base64"
+import "os"
+
+type Signer interface {
+  Sign([]byte)([]byte, os.Error)
+  Verify([]byte, []byte)(os.Error)
+}
+
+var SignatureVerificationFailed = os.NewError("Signature Verification Failed")
+
+// Sign a string with a specified signer and base64 encoding
+func Sign64(s Signer, e *base64.Encoding, sts []byte)(out []byte, err os.Error){
+  sig, err := s.Sign(sts)
+  if err != nil { return }
+  out = make([]byte, e.EncodedLen(len(sig)))
+  wr := bytes.NewBuffer(out)
+  enc := base64.NewEncoder(e, wr)
+  defer enc.Close()
+  _, err = enc.Write(sig)
+  if err != nil { return }
+  return
+}
+
+func SignString(s Signer, n string)(out string, err os.Error){
+  bb := bytes.NewBufferString(n)
+  bo, err := s.Sign(bb.Bytes())
+  if err == nil { out = string(bo) }
+  return
+}
+
+func SignString64(s Signer, e *base64.Encoding, sts string)(out string, err os.Error){
+  bb := bytes.NewBufferString(sts)
+  bo, err := Sign64(s, e, bb.Bytes())
+  if err == nil { out = string(bo) }
+  return
+}
