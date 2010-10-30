6e22d484748ddd1319279d15eeba24e10bd40dd3
James D. Nurmi <james@abneptis.com>
2010-10-30 16:21:36 -0700

    - Add function documentation - Add copyright blob

--- signer/signer.go
+++ signer/signer.go
@@ -1,9 +1,15 @@
 package signer
 
+/* 
+  Copyright (c) 2010, Abneptis LLC.
+  See COPYRIGHT and LICENSE for details.
+*/
+
 import "bytes"
 import "encoding/base64"
 import "os"
 
+/* A simple interface for signing and verifying HMAC signatures */
 type Signer interface {
   Sign([]byte)([]byte, os.Error)
   Verify([]byte, []byte)(os.Error)
@@ -20,6 +26,8 @@ func Sign64(s Signer, e *base64.Encoding, sts []byte)(out []byte, err os.Error){
   return
 }
 
+// Executes Sign(), however uses strings rather than the native
+// []byte types.
 func SignString(s Signer, n string)(out string, err os.Error){
   bb := bytes.NewBufferString(n)
   bo, err := s.Sign(bb.Bytes())
@@ -27,6 +35,8 @@ func SignString(s Signer, n string)(out string, err os.Error){
   return
 }
 
+// Return a signature encoded in base64 (with the encoding
+// specified by the caller)
 func SignString64(s Signer, e *base64.Encoding, sts string)(out string, err os.Error){
   bb := bytes.NewBufferString(sts)
   bo, err := Sign64(s, e, bb.Bytes())
