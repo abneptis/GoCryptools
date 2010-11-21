004b0c2a8fe027864dcb165e4f9a5dc971592b8d
James D. Nurmi <james@abneptis.com>
2010-11-21 12:19:05 -0800

    Break out verifier for public-key only usage

--- signer/pkcs15.go
+++ signer/pkcs15.go
@@ -6,8 +6,19 @@ import "crypto/rand"
 import "crypto/rsa"
 import "os"
 
+func PKCS15KnownMechs()([]string){
+  return []string{
+    "md5","sha1","sha256","sha384","sha512","md5sha1",
+  }
+}
+
 type PKCS15Signer struct {
   PrivateKey *rsa.PrivateKey
+  ID string
+}
+
+type PKCS15Verifier struct {
+  PublicKey *rsa.PublicKey
   ID string
 }
 
@@ -15,15 +26,14 @@ func NewPKCS15Signer(id string, pk *rsa.PrivateKey)(SignerMultiMech){
   return PKCS15Signer{PrivateKey: pk, ID: id}
 }
 
-func (self PKCS15Signer)KnownMechs()([]string){
-  return []string{
-    "md5","sha1","sha256","sha384","sha512","md5sha1",
-  }
+func NewPKCS15Verifier(id string, pk *rsa.PublicKey)(VerifierMultiMech){
+  return PKCS15Verifier{PublicKey: pk, ID: id}
 }
 
-func (self PKCS15Signer)SignerID()(string){
-  return self.ID
-}
+func (self PKCS15Signer)KnownMechs()([]string){ return PKCS15KnownMechs() }
+func (self PKCS15Verifier)KnownMechs()([]string){ return PKCS15KnownMechs() }
+func (self PKCS15Signer)SignerID()(string){ return self.ID }
+func (self PKCS15Verifier)SignerID()(string){ return self.ID }
 
 func (self PKCS15Signer)Sign(mech string, in []byte)(out []byte, err os.Error){
   err = ValidMech(mech, self)
@@ -41,6 +51,11 @@ func (self PKCS15Signer)Sign(mech string, in []byte)(out []byte, err os.Error){
 }
 
 func (self PKCS15Signer)Verify(mech string, sig []byte, sigo []byte)(err os.Error){
+  vo := NewPKCS15Verifier(self.ID, &self.PrivateKey.PublicKey)
+  return vo.Verify(mech,sig,sigo)
+}
+
+func (self PKCS15Verifier)Verify(mech string, sig []byte, sigo []byte)(err os.Error){
   err = ValidMech(mech, self)
   if err != nil { return }
   h, err := hashes.GetHash(mech)
@@ -51,7 +66,7 @@ func (self PKCS15Signer)Verify(mech string, sig []byte, sigo []byte)(err os.Erro
   //log.Printf("Verify Hash: %X", hbytes)
   hi, err := hashes.GetPKCS15Hash(mech)
   if err != nil { return }
-  err = rsa.VerifyPKCS1v15(&self.PrivateKey.PublicKey, hi, hbytes, sig)
+  err = rsa.VerifyPKCS1v15(self.PublicKey, hi, hbytes, sig)
   return
 }
 
